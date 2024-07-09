1930a6ffd1d94199df1b3dd5c7e875f6080b4663
llunasanz <lluna@declarando.es>
2024-07-09 20:25:51 +0200

    Improved db connection and db creation

--- src/connect_airflow_to_db.sh
+++ src/connect_airflow_to_db.sh
@@ -7,11 +7,17 @@ export $(grep -v '^#' .env | xargs)
 POSTGRES_PORT=$(docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Ports}}" | awk '$2 ~ /postgres/ {split($3, a, ":"); split(a[2], b, "-"); print b[1]}')
 
 # Construct the connection URI using the environment variables and the dynamically obtained port number
-CONN_URI="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:${POSTGRES_PORT}/${POSTGRES_DB}"
+CONN_URI="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
 # Define the Airflow webserver container name
 AIRFLOW_CONTAINER_NAME="ecommerce_airflow_db-airflow-webserver-1"
 
 # Add the connection to Airflow using Docker exec
-docker exec -it $AIRFLOW_CONTAINER_NAME airflow connections add 'my_prod_db' --conn-uri "${CONN_URI}"
-
+docker exec -it $AIRFLOW_CONTAINER_NAME airflow connections delete 'my_prod_db' || true
+docker exec -it $AIRFLOW_CONTAINER_NAME airflow connections add 'my_prod_db' \
+  --conn-type 'postgres' \
+  --conn-host "${POSTGRES_HOST}" \
+  --conn-schema "${POSTGRES_DB}" \
+  --conn-login "${POSTGRES_USER}" \
+  --conn-password "${POSTGRES_PASSWORD}" \
+  --conn-port "${POSTGRES_PORT}"
